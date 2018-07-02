5e884b87073fb357cf448c21a188ec37d550677c
Allison <allidomm@seas.upenn.edu>
2018-07-02 14:27:54 +0200

    list and item controllers

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,2 +1,44 @@
 export * from './ping.controller';
 export * from './user.controller';
+import { get, param, requestBody, post } from "@loopback/rest";
+import { repository } from "@loopback/repository";
+import { ItemRepository } from "../repositories/Item.repository";
+import { Class, Repository, RepositoryMixin, juggler } from '@loopback/repository';
+import { Item } from "../models/Item";
+
+
+// Uncomment these imports to begin using these cool features!
+
+// import {inject} from '@loopback/context';
+
+
+export class ItemController {
+
+  constructor(
+    @repository(ItemRepository.name) private itemRepo: ItemRepository
+  ) { }
+
+  @get("/items")
+  async getAllItems(
+    @param.query.string("itemId") ordername: string
+  ): Promise<Array<Item>> {
+    return await this.itemRepo.find();
+  }
+
+  @get("/item/itemname")
+  getSpecificItem(
+    @param.path.string("itemname") itemname: string
+  ): any {
+
+    return "Not found";
+
+  }
+
+  @post("/item")
+  async createItem(
+    @requestBody() item: Item
+  ): Promise<Item> {
+    let createdItem = await this.itemRepo.create(item);
+    return createdItem;
+  }
+}
---
+++ src/controllers/list.controller.ts
@@ -0,0 +1,42 @@
+import { get, param, requestBody, post } from "@loopback/rest";
+import { repository } from "@loopback/repository";
+import { ListRepository } from "../repositories/List.repository";
+import { Class, Repository, RepositoryMixin, juggler } from '@loopback/repository';
+import { List } from "../models/List";
+
+
+// Uncomment these imports to begin using these cool features!
+
+// import {inject} from '@loopback/context';
+
+
+export class OrderController {
+
+  constructor(
+    @repository(ListRepository.name) private listRepo: ListRepository
+  ) { }
+
+  @get("/list")
+  async getAllOrders(
+    @param.query.string("listname") listname: string
+  ): Promise<Array<List>> {
+    return await this.listRepo.find();
+  }
+
+  @get("/list/listname")
+  getSpecificList(
+    @param.path.string("listname") listname: string
+  ): any {
+
+    return "Not found";
+
+  }
+
+  @post("/list")
+  async createList(
+    @requestBody() list: List
+  ): Promise<List> {
+    let createdList = await this.listRepo.create(list);
+    return createdList;
+  }
+}
