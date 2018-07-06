57390d7b959d188582b6d13387d1e2ccd704faf3
Sierra Mills <sierratmills@gmail.com>
2018-07-06 11:46:40 +0200

    fixed merge conflict

--- migrations/20180705101546-item.js
+++ migrations/20180705101546-item.js
@@ -14,43 +14,6 @@ exports.setup = function(options, seedLink) {
   seed = seedLink;
 };
 
-<<<<<<< HEAD
-exports.up = function(db, callback) {
-  db.createTable('item', { 
-    id: {         
-      type: 'int',         
-      primaryKey: true       
-    },       
-    itemname: {         
-      type: 'string',         
-      length: 45       
-    },       
-    itemtype: {         
-      type: 'string',         
-      length: 45       
-    },        
-    price: {         
-      type: 'string',         
-      length: 45       
-    }, 
-    url: {         
-      type: 'string',         
-      length: 45       
-    },    
-    storeid: {         
-      type: 'string',         
-      length: 45       
-    },   
-    image: {         
-      type: 'string',         
-      length: 45       
-    },  
-    size: {         
-      type: 'string',         
-      length: 45       
-    },        
-  }, callback);
-=======
 exports.up = function (db, callback) {
   db.createTable('item', {
     id: {
@@ -96,7 +59,6 @@ exports.up = function (db, callback) {
   }, callback);
 
 
->>>>>>> 7893d7c97e5dffd20ca54606c6d182e9aec0f2ab
 };
 
 exports.down = function(db, callback) {
