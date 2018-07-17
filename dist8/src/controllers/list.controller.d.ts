037aebe381717170ddcae0606945cea5b40ca920
Allison <allidomm@seas.upenn.edu>
2018-07-17 13:39:18 +0200

    remove dist8

--- dist8/src/controllers/list.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { ListRepository } from "../repositories/List.repository";
-import { List } from "../models/List";
-export declare class OrderController {
-    private listRepo;
-    constructor(listRepo: ListRepository);
-    getAllOrders(listname: string): Promise<Array<List>>;
-    getSpecificList(listname: string): any;
-    createList(list: List): Promise<List>;
-}
--- dist8/src/repositories/List.repository.d.ts
+++
@@ -1,7 +0,0 @@
-import { DefaultCrudRepository } from '@loopback/repository';
-import { DataSource } from 'loopback-datasource-juggler';
-import { List } from '../models/list';
-export declare class ListRepository extends DefaultCrudRepository<List, typeof List.prototype.id> {
-    protected datasource: DataSource;
-    constructor(datasource: DataSource);
-}
